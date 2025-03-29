c6afc411fa72a27feb4f327348160ee2dbd145f7
SamrKat <ssethura@stevens.edu>
2025-03-29 15:30:32 -0400

    update

--- app/src/main/java/com/example/billsplitter/entities/Item.java
+++ app/src/main/java/com/example/billsplitter/entities/Item.java
@@ -1,4 +1,13 @@
 package com.example.billsplitter.entities;
 
 public class Item {
+    private String item_name;
+    private double item_cost;
+    private String user_id;
+
+    public item(String in, double c, String uid) {
+        this.item_name = in;
+        this.item_cost = c;
+        this.user_id = uid;
+    }
 }
