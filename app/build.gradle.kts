259b02786011e28699d911b56f391d6efa8e4250
TheDarkElites <73414180+TheDarkElites@users.noreply.github.com>
2025-03-29 17:42:01 -0400

    implement gradle stuff for Tesseract

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     annotationProcessor(libs.room.compiler)
+    implementation(libs.tess4j)
 }
