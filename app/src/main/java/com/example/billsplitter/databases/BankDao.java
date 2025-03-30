118f9ef0fd7464592ca25aeba07520f69c2879c0
SamrKat <ssethura@stevens.edu>
2025-03-29 22:22:38 -0400

    Update BankDao.java
    Put a few query methods in here, not a lot because some parameters are private.

--- app/src/main/java/com/example/billsplitter/databases/BankDao.java
+++ app/src/main/java/com/example/billsplitter/databases/BankDao.java
@@ -17,9 +17,43 @@ import java.util.List;
 @Dao
 public abstract class BankDao {
 
+    // SELECT ALL BANKING INFORMATION IN THE DATABASE
     @Query("SELECT * FROM bank")
     public abstract LiveData<List<Bank>> fetchAllBankingInfo();
 
+    // SELECT A USERS BANKING INFORMATION BASED ON THEIR BANKID
+    @Query("SELECT * FROM bank WHERE bankId = :bankId")
+    public abstract Bank fetchBankingWithBankId(int bankId);
+
+    // SELECT A USERS BANKING INFORMATION BASED ON THEIR USERID
+    @Query("SELECT * FROM bank WHERE bankerUserId = :userId")
+    public abstract Bank fetchBankingWithUserId(int userId);
+
+    // INSERT NEW USER BANKING INFORMATION INTO THE DATABASE
+    @Insert
+    protected abstract void insert(Bank bank);
+
+    // UPDATE EXISTING USER BANKING INFORMATINO IN THE DATABASE
+    @Update
+    protected abstract void update(Bank bank);
+
+    /**
+     * INSERTS THE BANKING INFORMATION TO THE DATABASE IF ID IS NOT VALID
+     * UPDATES THE BAKNING INFORMATINO IN THE DATABASE IF ID IS VALID
+     * @param bank
+     */
+    @Transaction
+    public void upsert(Bank bank) {
+        int id = bank.bankId;
+        if (id == 0) {
+            insert(bank);
+        }
+        else {
+            bank.bankId = id;
+            update(bank);
+        }
+
+    }
 
 
 }
