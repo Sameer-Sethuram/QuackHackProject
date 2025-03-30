25a3891ee65099916212d8ef5bbd59d3f1389b56
Jacob Choi <jake.d.choi@gmail.com>
2025-03-29 20:45:29 -0400

    OnClick

--- app/src/main/java/com/example/billsplitter/activities/RegisterUsersActivity.java
+++ app/src/main/java/com/example/billsplitter/activities/RegisterUsersActivity.java
@@ -1,16 +1,18 @@
 package com.example.billsplitter.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View.OnClickListener;
 
 import com.example.billsplitter.R;
 
-public class RegisterUsersActivity extends AppCompatActivity {
+public class RegisterUsersActivity extends AppCompatActivity implements OnClickListener {
 
     final static private String TAG = RegisterUsersActivity.class.getCanonicalName();
 
@@ -27,6 +29,10 @@ public class RegisterUsersActivity extends AppCompatActivity {
             return insets;
         });
 
+
+    }
+    @Override
+    public void onClick(View v){
 
     }
 }
