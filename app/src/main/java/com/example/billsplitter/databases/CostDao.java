7e0daf90cbb3a58d0501b40ad4a02ea43296b307
SamrKat <ssethura@stevens.edu>
2025-03-29 21:01:07 -0400

    QueryBackendChanges

--- app/src/main/java/com/example/billsplitter/databases/CostDao.java
+++ app/src/main/java/com/example/billsplitter/databases/CostDao.java
@@ -8,6 +8,18 @@ import androidx.room.Query;
 import androidx.room.Transaction;
 import androidx.room.Update;
 
+import com.example.billsplitter.entities.Cost;
+
+import java.util.List;
 @Dao
 public abstract class CostDao {
+
+    @Query("SELECT * FROM cost");
+    public abstract LiveData<List<Cost>> fetchAllCosts();
+
+    @Query("SELECT * FROM cost WHERE billId = :receipt")
+    public abstract LiveData<List<Cost>> fetchCostsFromBill(int receipt);
+
+
+
 }
--- app/src/main/java/com/example/billsplitter/databases/UserDao.java
+++ app/src/main/java/com/example/billsplitter/databases/UserDao.java
@@ -8,11 +8,15 @@ import androidx.room.Query;
 import androidx.room.Transaction;
 import androidx.room.Update;
 
+import com.example.billsplitter.entities.User;
+
+import java.util.List;
+
 @Dao
 public abstract class UserDao {
 
-    @Query("SELECT * FROM user");
+    @Query("SELECT * FROM user")
     public abstract LiveData<List<User>> fetchAllUsers();
 
-    
+
 }
